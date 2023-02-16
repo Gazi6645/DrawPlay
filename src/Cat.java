00bd9d24923e06ac1a6e6d50f5f34610f7cb04cc
Gazi M. and Annie H <gmdiftakhermorshed@mines.edu>
2023-02-16 13:44:34 -0700

    Changed size of head and eye. Driver Gazi and navigator Annie

--- src/Cat.java
+++ src/Cat.java
@@ -5,14 +5,14 @@ import java.awt.geom.*;
 public class Cat {//Cat Class
 	// drawing constants are private - no one needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 200;
+	private static final int HEAD_DIMENSION = 400;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/2;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 40;
-	private static final int EYE_WIDTH = 20;
+	private static final int EYE_HEIGHT = 80;
+	private static final int EYE_WIDTH = 40;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 20;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
